d95dc334a172ecfd14e6df640ce3b09703db5788
juansuarez2011-png <juansuarez.2011@gmail.com>
2026-04-01 09:47:09 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,12 @@
-const CACHE_NAME = 'syntro-cache-v1';
 const assets = [
   './',
-  './index.html',        // Tu GPSSYNTRO.html renombrado
-  './logo.png',          // El logo que subiremos como archivo
+  './index.html',
+  './logo.png',          // El rectangular para el encabezado
+  './icono.png',         // El cuadrado para el icono de la App
+  './manifest.json',    // ¡Crucial para la instalación!
   './leaflet.css',
   './leaflet.js',
   './turf.min.js',
   './proj4.js',
-  './togeojson.js'       // ¡Indispensable para procesar los KML!
+  './togeojson.js'
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
